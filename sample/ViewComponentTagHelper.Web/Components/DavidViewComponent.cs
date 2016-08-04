06725b5063cb71f979c52b871a0dd09431275a7b
Crystal Qian <crystaljqian@gmail.com>
2016-08-04 11:00:39 -0700

    Before reworking type->descriptor

---
+++ sample/ViewComponentTagHelper.Web/Components/DavidViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+//namespace ViewComponentTagHelper.Web.Components
+namespace ViewComponentTagHelper.Web
+{
+    public class DavidViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string money, string power)
+        {
+            this.GetType().GetTypeInfo();
+            return View("David", new { money, power });
+        }
+    }
+}
